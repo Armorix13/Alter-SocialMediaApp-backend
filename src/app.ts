49950191782a662e6e7d21b60e498f73bee00737
IzRohitoP <rohitreso13@gmail.com>
2024-12-18 22:48:03 +0530

    cors allow

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,29 @@ import 'dotenv/config';
 import morgan from 'morgan';
 import router from './routes';
 import path from 'path';
+import cors from "cors";
 import errorHandler from './middleware/errorHandler';
 const app: Application = express();
+
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(cors({
+  origin: '*',
+}));
 connectDB();
 //upload
-app.use("/uploads",express.static(path.join(__dirname, '../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, '../uploads')));
 
 // Routes
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-app.use('/api/v1',router);
-app.use("*", (req:Request, res:Response) => {
+app.use('/api/v1', router);
+app.use("*", (req: Request, res: Response) => {
   res.status(404).json({ success: false, message: "Route not found" });
 });
 app.use(errorHandler)
