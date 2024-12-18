6d8b8e623082197ce810f8e355f9f793e619064d
IzRohitoP <rohitreso13@gmail.com>
2024-12-18 23:48:06 +0530

    changes in  the social login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,21 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import { generateToken, } from "../utils/helpers";
 import { ERROR, SUCCESS } from "../utils/response";
 import UserModel from "../models/user.model";
-import { IRegisterUser,IUpdateUserData} from "../types/Request Body/User/types";
+import { IRegisterUser, IUpdateUserData } from "../types/Request Body/User/types";
 import { IdeleteAccountQueryType, IgetProfileQueryType } from "../types/Request Query/User/types";
 
-export const socail_login = async (req: Request<{},{},IRegisterUser>, res: Response, next: NextFunction): Promise<any> => {
+export const socail_login = async (req: Request<{}, {}, IRegisterUser>, res: Response, next: NextFunction): Promise<any> => {
     try {
-        let {fullName, email, socialType, socialId } = req.body;
+        let { fullName, email, socialType, socialId, isEmailVerified, profileImage } = req.body;
         console.log("req.body", req.body)
-        let userExists = await UserModel.findOne({socialId});
+        let userExists = await UserModel.findOne({ socialId });
         if (!userExists) {
             userExists = await UserModel.create({
-                fullName, email,socialType,socialId
+                fullName, email, socialType, socialId, isEmailVerified, profileImage
             })
         }
         // await sendEmail(email,1,otp)
-        const token = generateToken(userExists._id )
+        const token = generateToken(userExists._id)
         return SUCCESS(res, 200, "Login suceesfully", { userExists, token });
     } catch (error: any) {
         console.log(error)
@@ -26,11 +26,11 @@ export const socail_login = async (req: Request<{},{},IRegisterUser>, res: Respo
 
 
 //getProfile
-export const getProfile = async (req: Request<{},{},{},IgetProfileQueryType>, res: Response, next: NextFunction):Promise<any>=>{
+export const getProfile = async (req: Request<{}, {}, {}, IgetProfileQueryType>, res: Response, next: NextFunction): Promise<any> => {
     try {
         let userExists = req.user;
         const id = req.query.id;
-        if(id){
+        if (id) {
             userExists = await UserModel.findById(id);
 
         }
@@ -38,43 +38,43 @@ export const getProfile = async (req: Request<{},{},{},IgetProfileQueryType>, re
     } catch (error: any) {
         return ERROR(res, 500, "Internal Server Error", error);
     }
-    
+
 }
 //delete account
 
-export const deleteAccount = async (req: Request<{},{},{},IdeleteAccountQueryType>, res: Response, next: NextFunction): Promise<any>=>{
+export const deleteAccount = async (req: Request<{}, {}, {}, IdeleteAccountQueryType>, res: Response, next: NextFunction): Promise<any> => {
     try {
         let id = req.query.id || req.userId;
-        const type = req.query.type ;
+        const type = req.query.type;
         const userExits = await UserModel.findById(id);
         if (!userExits) {
             return ERROR(res, 400, "User not found", {});
-            }
-        if(type == 1){
-            userExits.isDeleted=true;
+        }
+        if (type == 1) {
+            userExits.isDeleted = true;
             await userExits.save();
-        }else{
+        } else {
             await UserModel.findByIdAndDelete(id);
-        
-    }
+
+        }
         return SUCCESS(res, 200, "User account deleted successfully");
     } catch (error: any) {
         return ERROR(res, 500, "Internal Server Error", error);
     }
 };
 //update usre data
-export const updateUserData = async (req: Request<{},{},IUpdateUserData>, res: Response, next: NextFunction):Promise<any> => {
+export const updateUserData = async (req: Request<{}, {}, IUpdateUserData>, res: Response, next: NextFunction): Promise<any> => {
     try {
         const id = req.userId;
         let userExists = await UserModel.findById(id);
         if (!userExists) {
             return ERROR(res, 400, "User not found", {});
         }
-        let {boi,fullName,} = req.body;
-      
+        let { boi, fullName, } = req.body;
+
         userExists.boi = boi || userExists.boi;
         userExists.fullName = fullName || userExists.fullName;
-        console.log("req.file",req.file)
+        console.log("req.file", req.file)
         if (req.files) {
             const files = req.files as { [fieldname: string]: Express.Multer.File[] };
 
@@ -95,7 +95,7 @@ export const updateUserData = async (req: Request<{},{},IUpdateUserData>, res: R
     }
 };
 //logout
-export const logout = async (req: Request, res: Response, next: NextFunction): Promise<any>=>{
+export const logout = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
         const id = req.userId;
         const user = await UserModel.findById(id);
@@ -103,10 +103,10 @@ export const logout = async (req: Request, res: Response, next: NextFunction): P
             return ERROR(res, 400, "User not found", {});
         }
         return SUCCESS(res, 200, "User logged out successfully", {});
-        
-    } catch (error:any) {
+
+    } catch (error: any) {
         next(error);
-        
+
     }
 
 }
--- src/types/Request Body/User/types.ts
+++ src/types/Request Body/User/types.ts
@@ -1,7 +1,6 @@
 export interface IRegisterUser {
-    fullName?: string; socialType?: number; socialId?: string; email: string; 
+    fullName?: string; socialType?: number; socialId?: string; email: string; isEmailVerified?: boolean; profileImage?: string;
 }
-
 export interface IUpdateUserData {
     fullName?: string;
     profileImage?: string;
